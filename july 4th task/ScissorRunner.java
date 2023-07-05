66109a884a7faa167f485aba0cd24322891ee3cb
B-C-Punith <bcpunith46@gmail.com>
2023-07-05 09:50:26 +0530

    task

---
+++ july 4th task/ScissorRunner.java
@@ -0,0 +1,16 @@
+class ScissorRunner 
+{
+    public static void main (String [] args)
+    {
+        Scissor ref1 = new Scissor("Nataraja");
+        Scissor ref2 = new Scissor("Nataraja",20);
+        Scissor ref3 = new Scissor("Nataraja",20,"Black");
+        Scissor ref4 = new Scissor("Nataraja",20,"Black","cutting");
+        Scissor ref5 = new Scissor("Nataraja",20,"Black","cutting","Cutting");
+       Scissor ref6 = new Scissor("Nataraja",20,"Black","cutting","Irom");
+        Scissor ref7 = new Scissor("Nataraja",20,"Black","cutting","Iron","Four");
+
+        
+
+    }
+}
